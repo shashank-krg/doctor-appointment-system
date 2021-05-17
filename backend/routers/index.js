ab51162f5434c6772dccf09fb65cd931fcc9240b
Shashank Kumar <63181812+shashanksanyasi@users.noreply.github.com>
2021-05-17 11:23:32 +0530

    Update index.js

--- backend/routers/index.js
+++ backend/routers/index.js
@@ -5,14 +5,16 @@ const userRoutes = require("./userRoutes");
 const slotRoutes = require("./slotRoutes");
 
 
+
 const router = express.Router();
 
-//to run in postman http://localhost:3011/api/appointment
+//to run in postman http://localhost:4000/api/appointment
 // getting data and posting data in db is at same route.
 
 router.use("/appointment", appointmentRoutes);  
 router.use("/slot", slotRoutes);
 router.use("/user", userRoutes);
+
 
 
 module.exports = router;
